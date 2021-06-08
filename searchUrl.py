79d3e5105e45b78f894903a1a45bd8535f99ac64
mrzhaoyixin <845209277@qq.com>
2021-06-08 11:26:40 +0800

    Update searchUrl.py

--- searchUrl.py
+++ searchUrl.py
@@ -1,13 +1,13 @@
 import db,time
 from ftplib import FTP_TLS
-FTPSERVERADDR = "111.63.118.135"
+FTPSERVERADDR = "ip"
 def getTimestamp():
   return time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
   
 def send2FTP(path,filename):
   with FTP_TLS(host = FTPSERVERADDR) as ftp:
     ftp.set_debuglevel(2)
-    ftp.login(user = "u0_a179", passwd = "Flzx3qc!")
+    ftp.login(user = "u0_a179", passwd = "passwd")
     ftp.pwd()
     #ftp.storbinary('STOR %s'%filename, open(path+filename, 'rb',8192)) 
     #ftp.cwd()
